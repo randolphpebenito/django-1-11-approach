5bb26cd81aed75392ea8abd01e224e9f11c4d6b9
Randolph Pe Benito <rpebenito@xyber.ph>
2017-12-27 22:39:35 +0800

    Finalizing the auth.

--- webapp/music/admin.py
+++ webapp/music/admin.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Album
+from .models import Album, Song
 
 # Register your models here.
 admin.site.register(Album)
+admin.site.register(Song)
